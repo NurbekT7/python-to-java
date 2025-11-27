88ab1cb3421813a85ca4044a5029662c0c305985
NurbekT7 <sh7nurbek@gmail.com>
2025-11-27 16:39:31 +0600

    average grade

---
+++ средний-балл.java
@@ -0,0 +1,19 @@
+public class GradeCheck {
+  public static void main(String args[]) {
+    int avg = 85;
+    String grade;
+    
+    if (avg >= 90) {
+        grade = "A" ;   
+    }else if (avg >= 80) {
+        grade = "B";
+    }else if (avg >= 70) {
+        grade = "C";
+    }else if (avg >= 60) {
+        grade = "D";
+    }else {
+        grade = "F";
+    }
+    System.out.println(avg + " = " + grade);
+  }
+}
